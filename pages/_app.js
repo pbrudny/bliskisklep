a2f3f023a7188fdaeb72d315e04864068ea50d45
Piotr Brudny <pbrudny@gmail.com>
2020-04-28 19:24:41 +0200

    Add env

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/global.css";
 import { config, library } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
 // See https://github.com/FortAwesome/react-fontawesome#integrating-with-other-tools-and-frameworks
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 library.add(
@@ -12,3 +11,4 @@ library.add(
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
